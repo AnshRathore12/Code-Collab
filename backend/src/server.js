deeb7686a534cbde8df63efc192033bc94f849ba
AnshRathore12 <anshrathore30@gmail.com>
2025-12-25 23:50:16 +0530

    Inngest Initialized

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,21 @@
 import express from "express"
 import path from "path"
 import {connectDB} from "./lib/db.js"
+import cors from "cors"
+import {serve} from "inngest/express"
+import {inngest} from "./lib/inngest.js"
 
 import {ENV} from "./lib/env.js"
 
 const app=express();
 const __dirname=path.resolve()
+
+// middleware
+app.use(express.json())
+// crediantials :true  meaning >> server allows a browser  to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL,credientials:true}))
+
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 app.get("/health",(req,res)=>{
   res.status(200).json({msg:"msg from api from server health checkpoint"})
