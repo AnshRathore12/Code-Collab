49dc2e743bd70a3f738aff3560d35fd18edf0603
AnshRathore12 <158604871+AnshRathore12@users.noreply.github.com>
2025-12-28 12:04:15 +0530

    Inngest import Change

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,8 @@ import path from "path"
 import {connectDB} from "./lib/db.js"
 import cors from "cors"
 import {serve} from "inngest/express"
-import {inngest} from "./lib/inngest.js"
-import {functions} from "./inngest/functions.js"
+import {inngest,functions} from "./lib/inngest.js"
+
 
 import {ENV} from "./lib/env.js"
 
